f10d91a208199c3123872fb9dea4db4cdeecf001
phissame <49441759+phissame@users.noreply.github.com>
2019-05-15 00:20:22 +0200

    AdopteUnSportWPF1.1

--- AdopteUnSportWPF/Menu.xaml.cs
+++ AdopteUnSportWPF/Menu.xaml.cs
@@ -24,10 +24,25 @@ namespace AdopteUnSportWPF
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnCommande_Click(object sender, RoutedEventArgs e)
         {
             Commande Cmd = new Commande();
             Cmd.ShowDialog();
+        }
+        private void btnProduit_Click(object sender, RoutedEventArgs e)
+        {
+            Produit Pdt = new Produit();
+            Pdt.ShowDialog();
+        }
+        private void btnLivraison_Click(object sender, RoutedEventArgs e)
+        {
+            Livraison Lvs = new Livraison();
+            Lvs.ShowDialog();
+        }
+        private void btnClient_Click(object sender, RoutedEventArgs e)
+        {
+            Client Clt = new Client();
+            Clt.ShowDialog();
         }
     }
 }

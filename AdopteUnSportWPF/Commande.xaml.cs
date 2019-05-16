052a163780bb1bd080e2f61e4c60445454782575
mattisterik <mattisterik@users.noreply.github.com>
2019-05-16 14:11:03 +0200

    AUS v2.1

--- AdopteUnSportWPF/Commande.xaml.cs
+++ AdopteUnSportWPF/Commande.xaml.cs
@@ -11,12 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace AdopteUnSportWPF
 {
-    /// <summary>
-    /// Logique d'interaction pour Commande.xaml
-    /// </summary>
+
     public partial class Commande : Window
     {
         public Commande()
@@ -26,7 +25,11 @@ namespace AdopteUnSportWPF
 
         private void OuiClient_Click(object sender, RoutedEventArgs e)
         {
-            
+                OuiClientID.IsEnabled = true;
+                OuiClientID.Opacity = 1;
+                NonClientID.IsEnabled = true;
+                NonClientID.Opacity = 1;
+                TextBlockQ.Opacity = 1;            
         }
 
         private void NonClient_Click(object sender, RoutedEventArgs e)
@@ -37,7 +40,12 @@ namespace AdopteUnSportWPF
         }
         private void OuiClientID_Click(object sender, RoutedEventArgs e)
         {
-
+            IDClientValider.IsEnabled = true;
+            IDClientValider.Opacity = 1;
+            TextBlockIDC.IsEnabled = true;
+            TextBlockIDC.Opacity = 1;
+            TextIDClient.Opacity = 1;
+            TextBlockIDC.Opacity = 1;
         }
 
         private void NonClientID_Click(object sender, RoutedEventArgs e)
@@ -45,9 +53,101 @@ namespace AdopteUnSportWPF
             
         }
 
+        private void IDClientValider_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            string IDClient = TextBlockIDC.Text;
+            bool ExistenceCli = ExistenceClient(IDClient);
+            if (ExistenceCli == true)
+            {
+                WrongC.Opacity = 0;
+                TrueC.Opacity = 1;
 
+                //InformationProduit(IDProduit);
+            }
+            else
+            {
+                WrongC.Opacity = 1;
+                TrueC.Opacity = 0;
+            }
+        }
+        private void btnAjouterP_Click(object sender, RoutedEventArgs e)
+        {
+            string IDProduit = TextBlockIDP.Text;
+            bool ExistenceProd = ExistenceProduit(IDProduit);
+            if (ExistenceProd == true)
+            {
+                WrongP.Opacity = 0;
+                TrueP.Opacity = 1;
+                if(ListeProd.Content != "")
+                {
+                    TextPanier.Content = "Voici les produits enregistrés :";
+                }
+                ListeProd.Content += IDProduit +" - ";
+                //InformationProduit(IDProduit);
+            }
+            else
+            {
+                WrongP.Opacity = 1;
+                TrueP.Opacity = 0;
+            }
+        }
+        private bool ExistenceClient(string IDClient)
+        {
+            bool Existence = false;
+            string infoConnexion = "SERVER = localhost; PORT = 3306; DATABASE = magasinAdopteUnSport; UID = root; PASSWORD = MATIbol78;";
+            MySqlConnection maConnexion = new MySqlConnection(infoConnexion);
+            maConnexion.Open();
+
+            MySqlCommand command = maConnexion.CreateCommand();
+            command.CommandText = "SELECT IDClients from Clients"; // exemple de requête
+
+            MySqlDataReader reader;
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    string ligne = reader.GetValue(i).ToString();
+                    if (ligne == IDClient)
+                    {
+                        Existence = true;
+                    }
+                }
+            }
+            maConnexion.Close();
+            return Existence;
+        }
+        private bool ExistenceProduit(string IDProduit)
+        {
+            bool Existence = false;
+            string infoConnexion = "SERVER = localhost; PORT = 3306; DATABASE = magasinAdopteUnSport; UID = root; PASSWORD = MATIbol78;";
+            MySqlConnection maConnexion = new MySqlConnection(infoConnexion);
+            maConnexion.Open();
+
+            MySqlCommand command = maConnexion.CreateCommand();
+            command.CommandText = "SELECT IDProduit from Produit"; // exemple de requête
+
+            MySqlDataReader reader;
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    string ligne = reader.GetValue(i).ToString();
+                    if (ligne == IDProduit)
+                    {
+                        Existence = true;
+                        Console.WriteLine(" Le produit a été trouvé.");
+                    }
+                }
+            }
+            maConnexion.Close();
+            return Existence;
         }
     }
 }
